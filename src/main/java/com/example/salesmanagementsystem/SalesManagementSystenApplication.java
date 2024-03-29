bb7185c9b4ee9c09abc1816f02d5e69ab9bd0f81
Timileyin Adeleke <101255913+timo-shey@users.noreply.github.com>
2024-03-29 12:11:37 +0100

    boilerplate codes

---
+++ src/main/java/com/example/salesmanagementsystem/SalesManagementSystenApplication.java
@@ -0,0 +1,13 @@
+package com.example.salesmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesManagementSystenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesManagementSystenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/salesmanagementsystem/SalesManagementSystenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.salesmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesManagementSystenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
